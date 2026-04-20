92a41f851809b3a370d726606495145eb8dcdeb0
fatratra-png <hei.fatratra@gmail.com>
2026-04-20 14:08:36 +0300

    ref:add export into server.js

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-import http from 'http';
-import path from 'path';
-import fs from 'fs';
-
+import http from "http";
+import path from "path";
+import fs from "fs";
 
 const startServer = (port = 3000) => {
-    const DIST_DIR = './dist';
+  const DIST_DIR = "./dist";
 
+  // Création du serveur
+  const SERVER = http.createServer((request, response) => {
+    let filePath = request.url === "/" ? "/index.html" : request.url;
+    const fullPath = path.join(process.cwd(), DIST_DIR, filePath);
 
-    // Création du serveur
-    const SERVER = http.createServer((request, response) => {
-        let filePath = request.url === '/' ? '/index.html' : request.url;
-        const fullPath = path.join(process.cwd(), DIST_DIR, filePath);
+    fs.readFile(fullPath, (err, content) => {
+      if (err) {
+        response.writeHead(404);
+        response.end("Erreur 404 : Page introuvable !");
+        return;
+      }
+      // Détection extension
+      const EXT = path.extname(fullPath);
 
+      response.writeHead(200, {
+        "Content-Type": EXT === ".html" ? "text/html" : "text/plain",
+      });
 
-        fs.readFile(fullPath, (err, content) => {
-            if (err) {
-                response.writeHead(404);
-                response.end('Erreur 404 : Page introuvable !');
-                return;
-            }
-            // Détection extension
-            const EXT = path.extname(fullPath);
-
-
-            response.writeHead(200, { 'Content-Type': EXT === '.html' ? 'text/html' : 'text/plain' });
-
-
-             // Envoi du contenu
-            response.end(content);
-        });
+      // Envoi du contenu
+      response.end(content);
     });
+  });
 
-
-// Démarrage du serveur
-    SERVER.listen(port, () => {
-        console.log(`🌍 Serveur démarré sur http://localhost:${port}`);
-        console.log(`💡 Appuyez sur Ctrl+C pour arrêter.`);
-    });
+  // Démarrage du serveur
+  SERVER.listen(port, () => {
+    console.log(`🌍 Serveur démarré sur http://localhost:${port}`);
+    console.log(`💡 Appuyez sur Ctrl+C pour arrêter.`);
+  });
 };
 export { startServer };
